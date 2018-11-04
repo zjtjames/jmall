5b5790b865ee04711bd0bc3d2c79e016b95270ce
uzig305 <zjt_james@163.com>
2018-11-04 21:42:20 +0800

    learn spring ioc & configure junit

---
+++ src/test/java/com/jmall/ioc/interfaces/OneInterfaceImplTest.java
@@ -0,0 +1,12 @@
+package com.jmall.ioc.interfaces;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OneInterfaceImplTest {
+    @Test
+    public void hello() throws Exception {
+    }
+
+}
