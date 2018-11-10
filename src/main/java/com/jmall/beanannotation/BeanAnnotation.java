037758c3af66c251e6b80baa03da09f8265cc9cf
uzig305 <zjt_james@163.com>
2018-11-10 22:53:59 +0800

    learn 注解方式定义bean以及bean的scope

---
+++ src/main/java/com/jmall/beanannotation/BeanAnnotation.java
@@ -0,0 +1,21 @@
+/**
+ * created by Zheng Jiateng on 2018/11/10.
+ */
+package com.jmall.beanannotation;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope("prototype")
+@Component("guaguagua")
+public class BeanAnnotation {
+
+    public void say(String arg) {
+        System.out.println("BeanAnnotation: " + arg);
+    }
+
+    public void myHashCode() {
+        System.out.println("BeanAnnotation: " + this.hashCode());
+    }
+
+}
