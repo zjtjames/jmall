31547b28c3539450fa10e7ff5b7ca8d83f0970e9
uzig305 <zjt_james@163.com>
2018-12-25 18:38:30 +0800

    分类模块 day1

---
+++ src/main/java/com/jmall/controller/backend/CategoryManageController.java
@@ -0,0 +1,16 @@
+/**
+ * created by Zheng Jiateng on 2018/12/25.
+ */
+package com.jmall.controller.backend;
+
+import com.jmall.common.ServerResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/manage/category")
+public class CategoryManageController {
+
+//    public ServerResponse
+
+}
